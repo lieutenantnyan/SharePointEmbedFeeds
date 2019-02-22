84cafaa11d5f37852e0756d226474b859337d1f4
lieutenantnyan <47829298+lieutenantnyan@users.noreply.github.com>
2019-02-21 18:54:00 -0600

    Adding script files

---
+++ TwitterFeed.js
@@ -0,0 +1,116 @@
+<a class="twitter-timeline" data-dnt="true" href="https://twitter.com/TwitterHandle" data-widget-id="487630789744398336">Tweets by @TwitterHandle</a>
+<script>
+    ! function (d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0],
+            p = /^http:/.test(d.location) ? 'http' : 'https';
+        if (!d.getElementById(id)) {
+            js = d.createElement(s);
+            js.id = id;
+            js.src = p + "://platform.twitter.com/widgets.js";
+            fjs.parentNode.insertBefore(js, fjs);
+        }
+    }(document, "script", "twitter-wjs");
+</script>
+
+<script type="text/javascript">
+    function setUserAgent(window, userAgent) {
+        // Works on Firefox, Chrome, Opera and IE9+
+        if (navigator.__defineGetter__) {
+            navigator.__defineGetter__('userAgent', function () {
+                return userAgent;
+            });
+        } else if (Object.defineProperty) {
+            Object.defineProperty(navigator, 'userAgent', {
+                get: function () {
+                    return userAgent;
+                }
+            });
+        }
+        // Works on Safari
+        if (window.navigator.userAgent !== userAgent) {
+            var userAgentProp = {
+                get: function () {
+                    return userAgent;
+                }
+            };
+            try {
+                Object.defineProperty(window.navigator, 'userAgent', userAgentProp);
+            } catch (e) {
+                window.navigator = Object.create(navigator, {
+                    userAgent: userAgentProp
+                });
+            }
+        }
+    }
+
+    try {
+        setUserAgent(window,
+            "Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.10136"
+        );
+    } catch (e) {
+        console.log(e);
+    }
+</script>
+
+<!-- the Twitter a-element and the platform/js code goes here -->
+<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+
+ 
+
+
+<a class="twitter-timeline" data-dnt="true" href="https://twitter.com/hashtag/Virtual" data-widget-id="487630789744398336">Tweets by @FollowVirtual</a>
+<script>
+    ! function (d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0],
+            p = /^http:/.test(d.location) ? 'http' : 'https';
+        if (!d.getElementById(id)) {
+            js = d.createElement(s);
+            js.id = id;
+            js.src = p + "://platform.twitter.com/widgets.js";
+            fjs.parentNode.insertBefore(js, fjs);
+        }
+    }(document, "script", "twitter-wjs");
+</script>
+
+<script type="text/javascript">
+    function setUserAgent(window, userAgent) {
+        // Works on Firefox, Chrome, Opera and IE9+
+        if (navigator.__defineGetter__) {
+            navigator.__defineGetter__('userAgent', function () {
+                return userAgent;
+            });
+        } else if (Object.defineProperty) {
+            Object.defineProperty(navigator, 'userAgent', {
+                get: function () {
+                    return userAgent;
+                }
+            });
+        }
+        // Works on Safari
+        if (window.navigator.userAgent !== userAgent) {
+            var userAgentProp = {
+                get: function () {
+                    return userAgent;
+                }
+            };
+            try {
+                Object.defineProperty(window.navigator, 'userAgent', userAgentProp);
+            } catch (e) {
+                window.navigator = Object.create(navigator, {
+                    userAgent: userAgentProp
+                });
+            }
+        }
+    }
+
+    try {
+        setUserAgent(window,
+            "Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.10136"
+        );
+    } catch (e) {
+        console.log(e);
+    }
+</script>
+
+<!-- the Twitter a-element and the platform/js code goes here -->
+<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
